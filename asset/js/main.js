2f62de33135e63409885479aa3a65e092034bad5
ummehabibasayma18 <summehabiba8@gmail.com>
2024-05-12 14:58:49 +0600

    fixed counter error

--- asset/js/main.js
+++ asset/js/main.js
@@ -49,49 +49,49 @@ goTopBtn.addEventListener("click",() => {
 
 
 
-// why us sec progressbar
-const meter = document.getElementById("progress--circle");
-const meterProgress = document.getElementById("meter--progress");
-const meterProgress = document.getElementById("meter--progress1");
-const ranger = document.getElementById("meter--ranger");
-const insideText = document.getElementById("progress--text");
+// // why us sec progressbar
+// const meter = document.getElementById("progress--circle");
+// const meterProgress = document.getElementById("meter--progress");
+// const meterProgress = document.getElementById("meter--progress1");
+// const ranger = document.getElementById("meter--ranger");
+// const insideText = document.getElementById("progress--text");
 
-ranger.addEventListener("input", e => {
+// ranger.addEventListener("input", e => {
 
 
   
-  const rangeValue = e.target.value;
+//   const rangeValue = e.target.value;
   
-  meterProgress.innerText = `${rangeValue}%`;
-  insideText.textContent = `${rangeValue}%`;
-  meter.style.strokeDashoffset = 100 - rangeValue;
+//   meterProgress.innerText = `${rangeValue}%`;
+//   insideText.textContent = `${rangeValue}%`;
+//   meter.style.strokeDashoffset = 100 - rangeValue;
   
-  if (rangeValue === "0") {
-    meter.style.stroke = "none";
-  } else {
-    meter.style.stroke = "#28411B";
-  }
-});
+//   if (rangeValue === "0") {
+//     meter.style.stroke = "none";
+//   } else {
+//     meter.style.stroke = "#28411B";
+//   }
+// });
 
-ranger.addEventListener("wheel", e => {
-  e.preventDefault();
+// ranger.addEventListener("wheel", e => {
+//   e.preventDefault();
   
-  const isWheelPositive = () =>  e.deltaY > 0;
+//   const isWheelPositive = () =>  e.deltaY > 0;
   
-  if (isWheelPositive()) {
-    let rangerValue = +ranger.value;
-    ranger.value = rangerValue += 1;
-  } else {
-    ranger.value -= 1;
-  }
+//   if (isWheelPositive()) {
+//     let rangerValue = +ranger.value;
+//     ranger.value = rangerValue += 1;
+//   } else {
+//     ranger.value -= 1;
+//   }
   
-  // Trigger the above (:6) event listener manually  
-  ranger.dispatchEvent(new Event("input"));
-});
+//   // Trigger the above (:6) event listener manually  
+//   ranger.dispatchEvent(new Event("input"));
+// });
 
-document.addEventListener("DOMContentLoaded", () => {
-  ranger.dispatchEvent(new Event("input"));
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   ranger.dispatchEvent(new Event("input"));
+// });
 
 // counter sec
 $(document).ready(function() {
@@ -108,4 +108,4 @@ $(document).ready(function() {
   });
   });
   
-  });
+});
